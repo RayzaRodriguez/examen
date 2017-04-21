e413fee418a4af8651fe31276a0bcff30e4ffd6d
RayzaRodriguez <rayza12_36@hotmail.com>
2017-04-21 12:56:24 -0500

    Add files via upload

---
+++ prac4.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <iostream>
+#include <cctype>
+
+using namespace std;
+
+/*char invertir(char lista1,char lista2)
+{
+    char temp1 = lista1[0];
+    for(int x=0;temp1!='\0',temp1=lista1[++x])
+    {
+        if(temp1=='\0')
+            return lista2;
+        else
+        {
+            lista2[x-1]=temp1;
+        }
+    }
+}*/
+
+int main()
+{
+    char lista1[]="maria";
+    //for(int x=0;x)
+    //char temp = lista[0];
+    //int x=0;
+    for(int x=0;lista[x]!='\0',lista[x++])
+        cout<<lista[x];
+        x++;
+    char lista2[x];
+    cout<<x;
+
+}
